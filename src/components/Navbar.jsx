875d35b03ee8f89e34c3dbefc4b10added27dad6
maryfiona <aguchidimmamaryfiona@gmail.com>
2024-07-26 05:40:06 +0100

    allitle mobile resp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
-import './Style.css'
+import './Style.css';
 import { Link } from 'react-router-dom';
 import TopNav from './TopNav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faInfoCircle, faTools, faEnvelope , faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faInfoCircle, faTools, faEnvelope, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
-// Use the URL of your online logo
 const logoUrl = 'https://static.wixstatic.com/media/170045_2dc6bcf5c7df43c79a49f31dca3c0e6d.png/v1/fill/w_218,h_83,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/170045_2dc6bcf5c7df43c79a49f31dca3c0e6d.png';
 
 const Navbar = () => {
@@ -16,46 +15,43 @@ const Navbar = () => {
           <Link className="navbar-brand" to="/">
             <img src={logoUrl} alt="GRANDEUR-TRUCK Logo" />
           </Link>
-  
-          <div className="collapse navbar-collapse" style={{ marginLeft: '15rem', color: 'white' }} id="navbarNav">
-            <ul className="navbar-nav ml-auto">
+
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <Link className="nav-link" to="/">
                   <FontAwesomeIcon icon={faHome} className="mr-1" style={{ color: '#3498db' }} /> Home
                 </Link>
               </li>
-
               <li className="nav-item">
                 <Link className="nav-link" to="/about">
                   <FontAwesomeIcon icon={faInfoCircle} className="fa-fw mr-1" style={{ color: '#e74c3c' }} /> About
                 </Link>
               </li>
-
               <li className="nav-item">
                 <Link className="nav-link" to="/service">
                   <FontAwesomeIcon icon={faTools} className="fa-fw mr-1" style={{ color: '#27ae60' }} /> Services
                 </Link>
               </li>
-
               <li className="nav-item">
                 <Link className="nav-link" to="/contact">
-                  <FontAwesomeIcon icon={faEnvelope} className="fa-fw mr-1  "  style={{ color: '#f39c12' }} /> Contact
+                  <FontAwesomeIcon icon={faEnvelope} className="fa-fw mr-1" style={{ color: '#f39c12' }} /> Contact
                 </Link>
               </li>
-
               <li className="nav-item">
                 <Link className="nav-link" to="/BookingForm">
-                  <FontAwesomeIcon icon={faShoppingCart} className="fa-fw mr-1  "  style={{ color: '#f39c12' }} /> Book Now
+                  <FontAwesomeIcon icon={faShoppingCart} className="fa-fw mr-1" style={{ color: '#f39c12' }} /> Book Now
                 </Link>
               </li>
-              {/* Add more links as needed */}
             </ul>
           </div>
         </div>
       </nav>
-      
     </div>
-   
   );
 };
 
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,59 +1,63 @@
-import React from 'react';
-import './Style.css'
+// src/Navbar.jsx
+import './Style.css';
+import { Link } from 'react-router-dom';
+import TopNav from './TopNav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faInstagram, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faHome, faInfoCircle, faTools, faEnvelope, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
-const TopNav = () => {
+const logoUrl = 'https://static.wixstatic.com/media/170045_2dc6bcf5c7df43c79a49f31dca3c0e6d.png/v1/fill/w_218,h_83,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/170045_2dc6bcf5c7df43c79a49f31dca3c0e6d.png';
+
+const Navbar = () => {
   return (
-    <div className="top-nav-container">
-      <div className="top-nav-item">
-        <FontAwesomeIcon icon={faEnvelope} className="icon" style={{ color: '#3498db' }} />
-        <a href="mailto:aguchidimmamaryfiona@gmail.com" className="custom-link">
-          aguchidimmamaryfiona@gmail.com
-        </a>
-      </div>
-      <div className="top-nav-item">
-        <FontAwesomeIcon icon={faFacebook} className="icon" style={{ color: '#3b5998' }} />
-        <a
-          href="https://www.facebook.com/FacebookName"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="custom-link"
-        >
-          FacebookName
-        </a>
-      </div>
-      <div className="top-nav-item">
-        <FontAwesomeIcon icon={faInstagram} className="icon" style={{ color: '#e4405f' }} />
-        <a
-          href="https://www.instagram.com/InstagramName"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="custom-link"
-        >
-          InstagramName
-        </a>
-      </div>
-      <div className="top-nav-item">
-        <FontAwesomeIcon icon={faTwitter} className="icon" style={{ color: '#1da1f2' }} />
-        <a
-          href="https://twitter.com/TwitterName"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="custom-link"
-        >
-          TwitterName
-        </a>
-      </div>
-      <div className="top-nav-item">
-        <FontAwesomeIcon icon={faWhatsapp} className="icon" style={{ color: '#25d366' }} />
-        <a href="https://wa.me/1234567890" target="_blank" rel="noopener noreferrer" className="custom-link">
-          +1234567890
-        </a>
-      </div>
+    <div>
+      <TopNav />
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <img src={logoUrl} alt="GRANDEUR-TRUCK Logo" />
+          </Link>
+  
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  <FontAwesomeIcon icon={faHome} className="mr-1" /> Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  <FontAwesomeIcon icon={faInfoCircle} className="mr-1" /> About
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/service">
+                  <FontAwesomeIcon icon={faTools} className="mr-1" /> Services
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  <FontAwesomeIcon icon={faEnvelope} className="mr-1" /> Contact
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/BookingForm">
+                  <FontAwesomeIcon icon={faShoppingCart} className="mr-1" /> Book Now
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
 
-export default TopNav;
+export default Navbar;
