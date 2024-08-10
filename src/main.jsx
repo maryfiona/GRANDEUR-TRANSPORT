176af3ef4562187478603968da02bc2973b037bd
maryfiona <aguchidimmamaryfiona@gmail.com>
2024-08-10 12:27:08 +0100

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx'
-import './index.css'
-
-
-
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
